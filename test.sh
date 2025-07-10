e6411e48e21a6048cbad47114eefd198886e35ed
guoh27 <hong.guo@advantech.com.cn>
2025-07-10 16:17:51 +0800

    docs: update build instructions

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+# build
+make clean >/dev/null
+make >/dev/null
+
+# list all sample cfgs
+for f in cfg/*.cfg; do
+    ./rkcfgtool "$f" > /dev/null
+done
+
+# create new cfg and read it back
+./rkcfgtool --create tmp.cfg --add foo bar > /dev/null
+./rkcfgtool tmp.cfg > /dev/null
+rm -f tmp.cfg
+
+echo "OK"
